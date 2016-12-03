bc5e8b5253c3f1986814d4039c8b1db303455174
lay <lyj051031448@163.com>
2016-12-03 14:36:55 +0800

    1，sql表 2，sql迁移

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
-
-# from app import app
-from app import views
+# this libs is a file in app dir
+from app import views, models
 
 
 
--- config.py
+++ config.py
@@ -1,2 +1,23 @@
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess-lgg'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+import os
+
+# get the dir path of this file(config.py)
+basedir = os.path.abspath(os.path.dirname(__file__))
+# __file__ = F:\python\flask-app\flask\config.py
+# os.path.dirname(__file__) = F:\python\flask-app\flask
+# os.path.abspath = 返回规范化的绝对路径。
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+print(basedir)
+
